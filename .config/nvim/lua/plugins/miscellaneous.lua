21fcfddfe747c0b6b2df3dd81c86db81e0f69c9d
AJ-Niro <andresninorodil@gmail.com>
2025-09-13 13:51:28 -0400

    feat(LazyVim): add miscellaneous with scrollbar

---
+++ .config/nvim/lua/plugins/miscellaneous.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "petertriho/nvim-scrollbar",
+    event = "BufReadPost",
+    opts = {
+      show = true,
+      show_in_active_only = true,
+      handlers = {
+        cursor = true,
+        diagnostic = true,
+        gitsigns = true,
+        search = true,
+      },
+    },
+  },
+}
