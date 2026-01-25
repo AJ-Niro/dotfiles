dba67690b5f3f472069bd0a03d34bad7df7f48d7
AJ-Niro <andresninorodil@gmail.com>
2026-01-25 16:07:50 -0400

    refactor(LazyVim): move scrollbar and theme config to miscellaneous.lua

--- .config/nvim/lua/plugins/miscellaneous.lua
+++ .config/nvim/lua/plugins/miscellaneous.lua
@@ -1,9 +1,37 @@
 return {
+  -- ===========================================================
+  -- Theme Selection
+  -- ===========================================================
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight-night",
+    },
+  },
+  -- ===========================================================
+  -- Spell Checking
+  -- ===========================================================
   {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
         "cspell-lsp",
+      },
+    },
+  },
+  -- ===========================================================
+  -- Display Scrollbar
+  -- ===========================================================
+  {
+    "petertriho/nvim-scrollbar",
+    event = "BufReadPost",
+    opts = {
+      show = true,
+      show_in_active_only = true,
+      handlers = {
+        cursor = true,
+        diagnostic = true,
+        gitsigns = true,
       },
     },
   },
